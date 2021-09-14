33cdc65302e911c12c84ece6a2cde958a2ba243e
SHIVAM-RAWAT/NLP/DATA SCIENCE <shivam.rawatece@gmail.com>
2021-09-15 01:20:18 +0530

    Optimization on SVM

---
+++ optimization_svm.py
@@ -0,0 +1,91 @@
+from data_preprocess import DataPreprocessing
+from regress_train import RegressionBase 
+from class_train import ClassificationBase 
+from grid_search import CogniGridSearch
+from bayesopt import CogniBayesOptimize
+from hoptimize import CogniHyperOptimize 
+import pickle
+import argparse 
+import warnings
+
+warnings.filterwarnings("ignore")
+
+class SvmOptimizeMethod:
+
+    def __init__(self,**kwargs):
+
+        if 'params' in kwargs:
+            self.params = kwargs['params']
+        if 'init_points' in kwargs:
+            self.init_points = kwargs['init_points']
+        if 'n_iter' in kwargs:
+            self.n_iter = kwargs['n_iter']
+        if 'max_evals' in kwargs:
+            self.max_evals = kwargs['max_evals']
+        if 'cross_val' in kwargs:
+            self.cross_val = kwargs['cross_val']
+        if  'model_path' in kwargs:
+            self.model_path = kwargs['model_path']
+    
+    # Calling Grid search for XGBoost
+    def grid_search(self):
+
+        obj_1 = CogniGridSearch(self.params,model_optimize = self.model_path)
+        obj_1.grid_search_cv(self.cross_val)
+    
+    # Calling Bayesian Optimization for XGBoost
+    def bayes_opt(self):
+    
+        obj_2 = CogniBayesOptimize(self.params,model_optimize = self.model_path)
+        obj_2.objective_bayes()
+        obj_2.bayesoptimize(self.init_points,self.n_iter)
+    
+    # Calling Hyper Optimization for XGBoost
+    def hyper_opt(self):
+        obj_3 = CogniHyperOptimize(self.params,model_optimize= self.model_path)
+        obj_3.objective_hopt(self.params)
+        obj_3.hyperopt(self.max_evals)
+    
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-degree", nargs = '+', type = int)
+    parser.add_argument("-max_iter", nargs = '+' , type = int)
+    parser.add_argument("-num_of_iter" , "--number_of_iteration" , help = 'used for bayesopt', nargs = '?',const = 10 , type = int, default = 5)
+    parser.add_argument("-init_points", "--number_of_initial_points", help = 'used for bayesopt', nargs = '?', const = 5, type = int, default = 5)
+    parser.add_argument("-max_evals", "--maximum_evaluations", help = 'used for hyperopt', nargs = '?', const = 20 , type = int , default = 20)
+    parser.add_argument("-cross_val", "--cross_validation", help = 'used for grid search', nargs = '?', const = 3, type = int, default = 3)
+    parser.add_argument("-o", "--option" , help = "enter the type of optimization method : gridsearch, bayesopt, hyperopt")
+
+    result = vars(parser.parse_args())
+
+    command = result.pop('option')
+
+    num_iter = result.pop('number_of_iteration')
+
+    init_points = result.pop('number_of_initial_points')
+
+    max_evals = result.pop('maximum_evaluations')
+
+    cross_val = result.pop('cross_validation')
+
+    param_list = {k : result[k] for k in result if result[k] != None}
+    
+    # model_path = "/home/congnitensor/Python_projects/model_class/file_logs/model_svm.txt"
+    model_path = "model.txt"
+
+    obj = SvmOptimizeMethod(params = param_list, init_points = init_points , 
+                            n_iter = num_iter , max_evals = max_evals, 
+                            cross_val= cross_val, model_path= model_path) 
+
+    if command == 'gridsearch':
+        obj.grid_search() 
+
+    if command == 'bayesopt':
+        obj.bayes_opt()
+    
+    if command == 'hyperopt':
+        obj.hyper_opt()
+
+
